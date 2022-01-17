67296e2e6c62b316bcc367d8f3d388468c4e331a
Hardik Kapadia <thecoderenroute@gmail.com>
2022-01-17 14:16:07 +0530

    Time: 1 ms (86.67%), Space: 37 MB (63.98%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        char[] chars = pattern.toCharArray();
+        String[] words = s.split(" ");
+
+        // System.out.println("Chars: " + Arrays.toString(chars));
+        // System.out.println("Words: " + Arrays.toString(words));
+
+        if (words.length != chars.length)
+            return false;
+
+        Map<Character, String> map = new HashMap<>();
+        Set<String> assigned = new HashSet<>();
+
+        for (int i = 0; i < chars.length; i++) {
+
+            // System.out.println("At " + i + ", the map is: " + map);
+
+            char c = chars[i];
+            String word = words[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word))
+                    return false;
+            } else if (assigned.contains(word)) {
+                return false;
+            } else {
+                map.put(c, word);
+                assigned.add(word);
+            }
+
+        }
+
+        return true;
+    }
+}
