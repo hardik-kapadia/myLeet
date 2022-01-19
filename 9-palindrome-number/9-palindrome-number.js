4be77e762c8b3d6c3b7dde15d855bbd07c1b47d9
Hardik Kapadia <thecoderenroute@gmail.com>
2022-01-19 19:44:15 +0530

    Time: 340 ms (20.41%), Space: 48.3 MB (49.35%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,21 +4,25 @@
  */
 var isPalindrome = function(x) {
     
-    if (x < 0)
+    if(x < 0)
         return false;
-
-    if (Math.floor(x) / 10 === 0)
+    
+    if(Math.floor(x/10) === 0)
         return true;
 
-    let num = x.toString();
-    let i = -1;
-    let l = num.length;
+    let y = x;
+    let z = 0;
 
-    while (++i < l / 2)
-        if (num.charAt(i) !== num.charAt(l - i - 1))
-            return false;
+    while(y > 0){
+        
+        let r = y % 10;
+        z = (z*10) + r;
+        y = Math.floor(y/ 10);
 
-    return true;
+    }
+    
+
+    return x === z;
     
     
 };
