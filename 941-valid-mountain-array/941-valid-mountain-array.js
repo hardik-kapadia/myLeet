af54221dc93291f82b5d25db9d8e3ddaf83a3618
Hardik Kapadia <thecoderenroute@gmail.com>
2022-01-25 20:48:36 +0530

    Time: 126 ms (21.22%), Space: 41.7 MB (68.28%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    
+    if (arr.length == 1)
+        return false;
+
+    let i;
+
+    for (i = 0; i < arr.length; i++)
+        if (arr[i] >= arr[i + 1])
+            break;
+    
+    if(i == 0 || i == arr.length)
+        return false;
+
+    for (; i < arr.length; i++)
+        if (arr[i] <= arr[i + 1])
+            return false;
+
+    return true;
+};
