188d3065a288d46e46e7e729adc2d2b74b1747be
Hardik Kapadia <thecoderenroute@gmail.com>
2022-01-30 14:14:21 +0530

    Time: 1 ms (76.47%), Space: 64.5 MB (5.22%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,23 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int l = nums.length;
-        k = k % l;
+
+        k = k >= l ? k % l : k;
 
         if (k == 0)
             return;
 
-        Queue<Integer> q = new ArrayDeque<>();
+        int[] result = new int[l];
 
-        int i;
-
-        for (i = 0; i < k; i++) {
-            q.add(nums[i]);
-            nums[i] = nums[l - k + i];
-        }
-
-        for (; i < nums.length; i++) {
-            q.add(nums[i]);
-            nums[i] = q.poll();
-        }
+        System.arraycopy(nums, l - k, result, 0, k);
+        System.arraycopy(nums, 0, result, k, l - k);
+        System.arraycopy(result, 0, nums, 0, l);
     }
 }
