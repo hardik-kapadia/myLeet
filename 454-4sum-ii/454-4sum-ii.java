03e444f7c3a857c93b9db9396d7dc8ca71cf7201
Hardik Kapadia <thecoderenroute@gmail.com>
2022-02-03 10:48:18 +0530

    Time: 214 ms (34.06%), Space: 53.3 MB (41.14%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,52 +1,17 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-        // Arrays.sort(nums1);
-        // Arrays.sort(nums2);
-        // Arrays.sort(nums3);
-        // Arrays.sort(nums4);
-
-        // corrsponds to a 2d array of int[nth array][currentSum] = count;
-        List<HashMap<Integer, Integer>> dp = new ArrayList<>();
-
-        for (int i = 0; i < 4; i++)
-            dp.add(new HashMap<>());
-
-        int count = helper(nums1, nums2, nums3, nums4, dp, 0, 0);
-
-        // System.out.println("dp: " + dp);
-
-        return count;
-    }
-
-    private int helper(int[] nums1, int[] nums2, int[] nums3, int[] nums4, List<HashMap<Integer, Integer>> dp,
-            int currentSum, int currentArray) {
-
-        // System.out.println("Now we have " + currentSum + " at " + currentArray + "
-        // with " + dp);
-
-        if (currentArray == 4)
-            return currentSum == 0 ? 1 : 0;
-
-        if (dp.get(currentArray).containsKey(currentSum))
-            return dp.get(currentArray).get(currentSum);
-
-        int[] nums = switch (currentArray) {
-            case 0 -> nums1;
-            case 1 -> nums2;
-            case 2 -> nums3;
-            case 3 -> nums4;
-            default -> null;
-        };
+        for (int i : nums1)
+            for (int j : nums2)
+                map.compute(i + j, (k, v) -> v == null ? 1 : v + 1);
 
         int count = 0;
 
-        for (int i = 0; i < nums.length; i++)
-            count += helper(nums1, nums2, nums3, nums4, dp, currentSum + nums[i], currentArray + 1);
-
-        dp.get(currentArray).put(currentSum, count);
+        for (int i : nums3)
+            for (int j : nums4)
+                count += map.getOrDefault(-(i + j), 0);
 
         return count;
-
     }
 }
