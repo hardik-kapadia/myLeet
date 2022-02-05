0a0449d43c0f6958127bcb233510d17dcd0ddd5b
Hardik Kapadia <thecoderenroute@gmail.com>
2022-02-05 12:15:58 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (5.28%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -10,7 +10,7 @@ class Solution {
         if(nums.length == 3)
             return Math.max(nums[1],nums[0]+nums[2]);
         
-        int[] dp = new int[nums.length+2];
+        int[] dp = new int[nums.length];
         
         dp[0] = nums[0];
         dp[1] = nums[1];
@@ -18,7 +18,6 @@ class Solution {
         
         for(int i=3;i < nums.length;i++)
             dp[i] = nums[i] + Math.max(dp[i-2],dp[i-3]);
-        
         
         return Math.max(dp[nums.length-2],dp[nums.length-1]);
         
