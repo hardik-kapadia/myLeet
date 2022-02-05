8cf9cbc113e17d0e36f48bc115e80d9c55c500cf
Hardik Kapadia <thecoderenroute@gmail.com>
2022-02-05 14:33:32 +0530

    Time: 14 ms (34.38%), Space: 44.1 MB (57.94%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,26 +1,33 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        if(nums.length == 1)
-            return true;
-        
-        boolean[] canReach = new boolean[nums.length];
-        canReach[0] = true;
-        
-        for(int i =0;i < nums.length -1;i++)
-        {
-            
-            if(!canReach[i])
-                continue;
-            
-            for(int j = i+1;j <= i+nums[i] && j < nums.length;j++)
-                canReach[j] = true;
-            
-            if(canReach[nums.length-1])
-                return true;
+
+        Map<Integer, Boolean> reachables = new HashMap<>();
+        reachables.put(0, true);
+
+        return canReach(nums, nums.length - 1, reachables);
+    }
+
+    boolean canReach(int[] nums, int index, Map<Integer, Boolean> reachables) {
+
+        // System.out.println("Now looking at: " + index);
+
+        if (reachables.containsKey(index))
+            return reachables.get(index);
+
+        int i = 1;
+
+        while (i <= index) {
+            if (nums[index - i] >= i)
+                if (canReach(nums, index - i, reachables)) {
+                    reachables.put(index - i, true);
+                    return true;
+                }
+            i++;
         }
-        
+
+        reachables.put(index, false);
+
         return false;
-        
+
     }
 }
