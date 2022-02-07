979314bc18d1c272e7ced00c86e87b6323f7a6bf
Hardik Kapadia <thecoderenroute@gmail.com>
2022-02-07 15:56:35 +0530

    Time: 4 ms (40.98%), Space: 42.3 MB (10.86%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        
+        for(int i=0;i<sc.length;i++)
+            if(sc[i] != tc[i])
+                return tc[i];
+        
+        
+        return tc[tc.length-1];
+        
+    }
+}
