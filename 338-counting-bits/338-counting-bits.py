bf03038cc3c7599f55805957a6a5112a7ff2e7c0
Hardik Kapadia <thecoderenroute@gmail.com>
2022-03-01 12:02:00 +0530

    Time: 105 ms (71.22%), Space: 20.7 MB (87.66%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,31 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if(n == 0):
-            return [0]
-
-        # dp = {0: 0, 1: 1, 2: 1}
-
-        @lru_cache
-        def count_for(num: int):
-
-            if(num == 0):
-                return 0
-
-            q = num//2
-            r = num % 2
-
-            temp = r
-
-            return count_for(q) + temp
-
-        ans = [0]
-
-        temp = 0b0
-        count = 0
-
-        for i in range(1, n+1):
-            ans.append(count_for(i))
-
-        # print(f'for {n} -> {ans}')
-
-        return ans
+        
+        numerOfOnesArr = [0]
+        while(len(numerOfOnesArr) < n + 1):
+            numerOfOnesArr.extend([x+1 for x in numerOfOnesArr])
+        return numerOfOnesArr[:n+1]
