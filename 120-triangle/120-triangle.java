c436065c116697c7691f105894a15d5e8d00cfb9
Hardik Kapadia <thecoderenroute@gmail.com>
2022-06-13 16:37:25 +0530

    Time: 8 ms (24.53%), Space: 44.6 MB (16.57%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        List<List<Integer>> dp = new ArrayList<>();
+        
+        int l = triangle.size();
+        
+        for(int i=0;i<l-1;i++)
+            dp.add(new ArrayList<>());
+        
+        dp.add(new ArrayList<>(triangle.get(l-1)));
+        
+        
+        for(int i = l-2;i>=0;i--){
+            
+            List<Integer> temp = dp.get(i);
+            List<Integer> temper = dp.get(i+1);
+            
+            for(int j = 0;j<=i;j++)
+                temp.add(j,triangle.get(i).get(j) + Math.min(temper.get(j),temper.get(j+1)));
+                
+            
+        }
+        
+        // System.out.println(dp);
+        
+        return dp.get(0).get(0);
+        
+    }
+}
