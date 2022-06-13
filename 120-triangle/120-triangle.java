9e61228b5d7c2d991f44649d959564549f19dd3a
Hardik Kapadia <thecoderenroute@gmail.com>
2022-06-13 16:53:17 +0530

    Time: 9 ms (19.93%), Space: 42.9 MB (74.98%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -11,18 +11,21 @@ class Solution {
         dp.add(new ArrayList<>(triangle.get(l-1)));
         
         
+        System.out.println(dp);
+        
+        
         for(int i = l-2;i>=0;i--){
             
             List<Integer> temp = dp.get(i);
             List<Integer> temper = dp.get(i+1);
             
             for(int j = 0;j<=i;j++)
-                temp.add(j,triangle.get(i).get(j) + Math.min(temper.get(j),temper.get(j+1)));
+                temp.add(triangle.get(i).get(j) + Math.min(temper.get(j),temper.get(j+1)));
                 
             
         }
         
-        // System.out.println(dp);
+        System.out.println(dp);
         
         return dp.get(0).get(0);
         
