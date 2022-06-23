2d39e3733a449499f0aafe7dec5b9e4d2397701e
Hardik Kapadia <thecoderenroute@gmail.com>
2022-06-23 10:34:12 +0530

    Time: 24 ms (17.21%), Space: 42.8 MB (69.77%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length() != t.length())
+            return false;
+        
+        Map<Character,Character> swap = new HashMap<>();
+        Set<Character> assigned = new HashSet<>();
+        
+        for(int i=0;i<s.length();i++){
+            
+            char cS = s.charAt(i);
+            char cT = t.charAt(i);
+            
+            if(swap.containsKey(cS)){
+                if(swap.get(cS) != cT)
+                    return false;
+            }
+            
+            if(assigned.contains(cT))
+                if(!swap.containsKey(cS) || swap.get(cS) != cT)
+                    return false;
+            
+            swap.put(cS,cT);
+            assigned.add(cT);
+            
+        }
+        
+        return true;
+        
+        
+        
+    }
+}
